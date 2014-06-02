08838f8afbb96fa8d2e6281835fdca86271ae1dc
Bart <bartosz.gryszko@gmail.com>
2014-06-02 18:22:13 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,41 @@
+=====
+reCAPTCHA Mailhide
+=====
+
+ReCAPTCHA Mailhide is an app for hiding mails from spammers. To use with Django templates.
+
+Installation
+-----------
+
+Install using PIP
+```
+pip uninstall django-recaptcha-mailhide
+```
+
+Add "recaptcha-mailhide" to your INSTALLED_APPS setting like this:
+
+```
+    INSTALLED_APPS = (
+     	...
+    	'recaptcha_mailhide',
+    )
+
+	MAILHIDE_PUBLIC_KEY = "XXX"
+	MAILHIDE_PRIVATE_KEY = "XXX"
+```
+
+You can obtain public and private key for free via http://www.google.com/recaptcha/mailhide/apikey
+
+Usage
+-----------
+
+First load it in Django template and use it as a filter.
+
+```
+{% load mailhide %}
+You can mail me at {{ "example@example"|mailhide }}
+```
+
+Credits
+-------------
+Adapted from https://github.com/jbzdak/django-mailhide
---
+++ templatetags/mailhide.py
@@ -0,0 +1,53 @@
+import base64
+import binascii
+from urllib.parse import urlencode
+
+from Crypto.Cipher import AES
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+from django.conf import settings
+from django import template
+
+register = template.Library()
+
+def pad_string (str, block_size):
+    numpad = block_size - (len (str) % block_size)
+    return str + numpad * chr (numpad)
+
+def enc_string(str):
+    key = binascii.unhexlify(settings.MAILHIDE_PRIVATE_KEY)
+    mode = AES.MODE_CBC
+    iv = '\000' * 16
+    obj = AES.new(key, mode, iv)
+    return obj.encrypt(str)
+
+
+@register.filter()
+def mailhide(value):
+
+    if value is None or not "@" in value:
+        return
+
+    args = {}
+    padded_value = pad_string(value, 16)
+    encrypted_value = enc_string(padded_value)
+
+    args['k'] = settings.MAILHIDE_PUBLIC_KEY
+    args['c'] = base64.urlsafe_b64encode(encrypted_value)
+
+    email = value.split("@")[0]
+
+    if len(email) >= 6:
+        prefix = email[0:3]
+    elif len(email) >= 4:
+        prefix = email[0]
+    else:
+        prefix=''
+
+    href = settings.MAILHIDE_API_URL + "?" + urlencode(args)
+
+    return mark_safe(render_to_string(("mailhide/email.html"), {
+        'domain': domain,
+        'prefix': prefix,
+        'captcha_url': href
+    }))
